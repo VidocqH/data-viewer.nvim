8f8a1a4e8d66b48b768e01b6ebe30a2c734aa4ea
Vidocq <vidocqho@gmail.com>
2023-09-29 14:51:42 +0800

    chore: add comment

--- lua/data-viewer/parser/parsers.lua
+++ lua/data-viewer/parser/parsers.lua
@@ -1,3 +1,7 @@
+-- Return a table { headers = headers, bodyLines = bodyLines }
+-- headers: string[]                  -- Contains all column names by display order
+-- bodyLines: table<string, string>[] -- Array of tables, each table represents a line by type {[columnName]: value}
+
 ---@class Parsers
 local M = {
   csv = require('data-viewer.parser.csv'),
